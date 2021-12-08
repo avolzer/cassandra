5a733049142576580b3d22d037043fb269e12dee
avolzer <50594927+avolzer@users.noreply.github.com>
2021-12-07 23:15:07 -0500

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './storySteps.json';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
+  console.log(data.steps[0]);
+  const [currQuestion, setCurrQuestion] = useState(data.steps[0])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{paddingLeft:100, paddingRight:100}}>
+          {currQuestion.question}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <div className="d-grid gap-2">
+  <Button variant="light" onClick={()=>{setCurrQuestion(data.steps[currQuestion.choices[0].goto])}}>
+  {currQuestion.choices[0].choice}
+  </Button>
+  {currQuestion.choices.length > 1 && 
+  <Button variant="secondary" onClick={()=>{setCurrQuestion(data.steps[currQuestion.choices[1].goto])}}>
+  {currQuestion.choices[1].choice}
+  </Button>
+}
+</div>
+        
+        
       </header>
     </div>
   );
